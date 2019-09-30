e95b4bfd191becabcfeeac171d9dcd7f7b46c1e5
Harsh Patni <37567574+patni-harsh5@users.noreply.github.com>
2019-09-30 15:11:32 -0500

    Update 2 commit

--- src/app/shared/recipedata.service.ts
+++ src/app/shared/recipedata.service.ts
@@ -6,6 +6,7 @@ export class RecipeData {
     private recipes: Recipe[] = [
         new Recipe('Schnitzel', 'Very tasty', 'https://natashaskitchen.com/wp-content/uploads/2016/02/Pork-Schnitzel-Recipe-5.jpg', new Date('09/27/2019'), new Date('10/12/2019'), 30, 100),
         new Recipe('Summer Salad', 'Okayish', 'http://images.media-allrecipes.com/userphotos/960x960/4552561.jpg', new Date('09/25/2019'), new Date('10/25/2019') , 20, 200)
+        // new Recipe('Hammer', 'For hitting nails', 'https://images-na.ssl-images-amazon.com/images/I/61fnMMgkGIL._SX425_.jpg', new Date('09/25/2019'), new Date('10/25/2019') , 20, 200)
     ];
     
     constructor() {}
